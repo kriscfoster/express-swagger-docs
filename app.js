f149040c2abf1d7e06bf0688b391fb55e0516f4a
kriscfoster <krisfoster@live.ie>
2020-09-06 11:12:07 +0100

    express swagger tods

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const app = express();
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: "Library API",
+      version: '1.0.0',
+    },
+  },
+  apis: ["index.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     description: Get all books
+ *     responses:
+ *       200:
+ *         description: Success
+ * 
+ */
+app.get('/books', (req, res) => {
+  res.send([
+    {
+      id: 1,
+      title: "Harry Potter",
+    }
+  ])
+});
+
+/**
+ * @swagger
+ * /books:
+ *   post:
+ *     description: Get all books
+ *     parameters:
+ *      - name: title
+ *        description: title of the book
+ *        in: formData
+ *        required: true
+ *        type: string
+ *     responses:
+ *       201:
+ *         description: Created
+ */
+app.post('/books', (req, res) => {
+  res.status(201).send();
+});
+
+/**
+ * @swagger
+ * /authors:
+ *   get:
+ *     description: Get all authors
+ *     responses:
+ *       200:
+ *         description: Success
+ * 
+ */
+app.get('/authors', (req, res) => {
+  res.send([
+    {
+      id: 1,
+      title: "John Smith",
+    }
+  ])
+});
+
+app.listen(5000, () => console.log("listening on 5000"));
